d8fd6c3a5abbcfbaa377a39e29d6aeac84b80113
jthorel <jthorel@kth.se>
2014-09-11 14:37:13 +0200

    HEJ

--- lab4-1.py
+++ lab4-1.py
@@ -26,22 +26,15 @@ print(forskjut)
 for i, s in enumerate(linesFile):
 	linelist = list(s)
 	for n,char in enumerate(linelist):
-<<<<<<< HEAD
-		linelist[n] = chr((ord(char)-ord('A')+forskjut) % 26+ord('A'))
-=======
 		if char.isupper():
 			linelist[n] = chr((ord(char)-ord('A')+forskjut) % 26+ord('A'))
->>>>>>> b62406f9b60053df23bee862756ad070a29a2302
 	linesFile[i] = ''.join(linelist)
 	
 print(linesFile)
 
 
-<<<<<<< HEAD
-=======
 	
 		
 
 
->>>>>>> b62406f9b60053df23bee862756ad070a29a2302
 writeFile(linesFile)
